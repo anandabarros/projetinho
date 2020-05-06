f5460f301cc72deb001ddb3c2cc71a89261ea5fe
Ananda Barros <anandakarenbn@alu.ufc.br>
2020-05-06 14:27:09 -0300

    comitando pra enviar

---
+++ scripts_index.js
@@ -0,0 +1,83 @@
+cont = 0
+function continuar() {
+  cont = 1 + cont
+  if(cont == 1){  
+    paragrafo.innerHTML = "Oiê meu amor!";
+  }
+  else if(cont == 2){
+    paragrafo.innerHTML = "Adivinha!";
+  }
+  else if(cont == 3){
+    paragrafo.innerHTML = "Hoje é dia 06 e eu não posso deixar passar em branco né,";
+  }
+  else if(cont == 4){
+    paragrafo.innerHTML = "mesmo longe eu tinha que achar uma forma de fazer uma coisinha especial pra ti,";
+  }
+  else if(cont == 5){
+    paragrafo.innerHTML = "então eu tive essa ideia e pensei porque não, então aqui estou!";
+  }
+  else if(cont == 6){
+    paragrafo.innerHTML = "Engraçado como tem horas que o tempo parece ter passado tão rápido";
+  }
+  else if(cont == 7){
+    paragrafo.innerHTML = "7 meses já...";
+  }
+  else if(cont == 8){
+    paragrafo.innerHTML = "mas as vezes parece que foi ontem que eu te beijei pela primeira vez.";
+  }
+  else if(cont == 9){
+    paragrafo.innerHTML = "7 meses parece tanto tempo e ao mesmo tempo tão pouco...";
+  }
+  else if(cont == 10){
+    paragrafo.innerHTML = "a gente já viveu e passou por tantas coisas nesse intervalo de tempo,";
+  }
+  else if(cont == 11){
+    paragrafo.innerHTML = "mas ao mesmo tempo eu queria ter vivido tanto mais,";
+  }
+  else if(cont == 12){
+    paragrafo.innerHTML = "é provavelmente por conta dessa droga de quarentena,";
+  }
+  else if(cont == 13){
+    paragrafo.innerHTML = "mas eu queria ter feito mais de tudo,";
+  }
+  else if(cont == 14){
+    paragrafo.innerHTML = "queria ter te beijado mais..";
+  }
+  else if(cont == 15){
+    paragrafo.innerHTML = "abraçado mais..";
+  }
+  else if(cont == 16){
+    paragrafo.innerHTML = "queria ter te olhado mais..";
+  }
+  else if(cont == 17){
+    paragrafo.innerHTML = "Hoje também tá fazendo um mês desde que te vi pela última vez,";
+  }
+  else if(cont == 18){
+    paragrafo.innerHTML = "a saudade tem horas que maltrata com força.";
+  }
+  else if(cont == 19){
+    paragrafo.innerHTML = "Oque nos traz pro motivo de eu ter feito isso aqui,";
+  }
+  else if(cont == 20){
+    paragrafo.innerHTML = "ao invés de ficar pulando de um app pro outro no celular olhando nossas coisas,";
+  }
+  else if(cont == 21){
+    paragrafo.innerHTML = "eu pensei em criar um lugarzinho nosso que reunisse nossa história.";
+  }
+  else if(cont == 22){
+    paragrafo.innerHTML = "Então é isso! Acho que já falei de mais aqui, vamos pro que realmente interessa.";
+  }
+  else if(cont == 23){
+    paragrafo.innerHTML = "Espero goste do presentinho, tô com muita saudades, te amo meu bem!";
+  }
+  else if(cont == 24){
+    paragrafo.innerHTML = "Seja bem vinda ao nosso universo!";
+  }
+  else if(cont == 25){
+    paragrafo.innerHTML = "E que possamos continuar expandindo ele juntas por muito mais tempo";
+  }
+  else if(cont == 26){
+    window.location.replace("pagina2.html");
+  }
+}
+
